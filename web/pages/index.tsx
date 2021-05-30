db24d3c646bc2f8f31edea5594f5ecbe9b4786d0
Alex Gladd <alex@alexgladd.com>
2021-05-30 21:31:16 +0200

    Add featured section to homepage

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,33 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { GetStaticProps } from 'next';
+import { sanityClient } from '../lib/sanity.server';
+import { SailboatDocument } from '../lib/types';
+import { featuredSailboatsForSale } from '../lib/queries';
+import FeaturedBoats from '../components/FeaturedBoats';
 
-export default function Home() {
+type HomeProps = {
+  featured: SailboatDocument[];
+}
+
+export default function Home({ featured }: HomeProps) {
   return (
     <Layout pageTitle="Home">
-      <div className="p-6 max-w-sm mx-auto my-4 bg-yellow-300 rounded-lg shadow-md flex justify-center">
-        Homepage!
-      </div>
+      <main className="space-y-8">
+        <FeaturedBoats boats={featured} />
+        <div className="p-6 max-w-sm mx-auto bg-yellow-300 rounded-lg shadow-md flex justify-center">
+          Homepage!
+        </div>
+      </main>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const featuredBoats = await sanityClient.fetch<SailboatDocument[]>(featuredSailboatsForSale);
+
+  return {
+    props: {
+      featured: featuredBoats,
+    }
+  };
 }
